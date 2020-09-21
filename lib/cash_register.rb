d241257e065e8a93b6dfd7b4b4fdc4e675b4dc38
Travis Lee <travisslee223@gmail.com>
2020-09-21 19:27:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,8 +3,9 @@ class CashRegister
   attr_accessor :total, :employee_discount
   
   def intialize(total, employee_discount)
+    
+    total = 0 
     @total = total
-    @total = 0 
     @employee_discount = total*0.8
   end
 end
