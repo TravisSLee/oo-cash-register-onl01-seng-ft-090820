9868b1b537f51709b8e286d5ad627a7c6ce04439
Travis Lee <travisslee223@gmail.com>
2020-09-21 19:28:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,6 +6,8 @@ class CashRegister
     
     total = 0 
     @total = total
-    @employee_discount = total*0.8
+    employee_discount = total*0.8
+    @employee_discount = employee_discount
+    
   end
 end
